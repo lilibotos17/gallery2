b381f48a47263a734751f2cf8c0b7004d805b78b
lili <@lili>
2024-05-02 08:36:31 +0200

    sikerult

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const kepek = ["delfin.png", "kutya.webp", "macska.jpg"];
 
 for (const kep of kepek) {
   const img = document.createElement("img");
-  img.src = "kepek/" + kep;
+  img.src = kep;
   img.width = "200";
   console.log("kep:" + img.src);
   container.appendChild(img);
+
   img.addEventListener("click", function () {
     console.log(this.src);
     targetImage.src = this.src;
